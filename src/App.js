9dafffb3c308744018f4bcae724e0ef5332088f7
Victoria Benoit <vrose834@gmail.com>
2023-08-15 16:22:27 -0300

    Added google fonts and started navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import NavBar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-     <h1>Jamming Spotify App</h1>
+      <NavBar />
+    
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+     <nav>
+        <h1>Jamming</h1>
+
+    </nav> );
+}
+ 
+export default NavBar;
