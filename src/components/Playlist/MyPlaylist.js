a8386381b8367e96c3f795f3c0033bd5abc00735
Victoria Benoit <vrose834@gmail.com>
2024-11-19 17:40:47 -0400

    Added in a delete the song

--- src/components/Playlist/MyPlaylist.js
+++ src/components/Playlist/MyPlaylist.js
@@ -3,12 +3,24 @@ import { MyPlaylistContainer } from "./MyPlaylist.styles";
 // Right Container - displays the users tracks and 
 // Implement a delete function
 const MyPlaylist = ({savePlaylist, status, playlistAlbums, playlistName, setPlaylistName, setPlaylistAlbums}) => {
+
+    const deleteSong =  (song) => {
+        const filtered = playlistAlbums.filter((p) => p.id !== song.id);
+        setPlaylistAlbums(filtered);
+    }
+
     return ( 
         <MyPlaylistContainer>
             <input type="text" placeholder="My Playlist" value={playlistName} onChange={(e) => setPlaylistName(e.target.value)}></input>
             <p>{status}</p>
             {playlistAlbums?.map((song) => {
-                return <p>{song?.name}</p>
+                return <div>
+                    <h3>{song.name}</h3>
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        deleteSong(song);
+                    }}>delete</button>
+                </div>
             })}
             <button onClick={(e) => {
                 e.preventDefault();
